965910a7f460e5aaa2807050b48834157cc88f06
鱼信 <wzf185325@alibabapictures.com>
2019-01-28 16:15:22 +0800

    修改获取群组接口

--- public/javascripts/db/db_group.js
+++ public/javascripts/db/db_group.js
@@ -40,7 +40,7 @@ function deleteGroup(uid,gid,callback){
 
 
 function myGroup(uid,callback){
-    let sql = " select picturegroup.* from picturegroup,relationship where relationship.gid = picturegroup.id && relationship.uid="+uid;
+    let sql = " select picturegroup.*,relationship.uid from picturegroup,relationship where relationship.gid = picturegroup.id && relationship.uid="+uid;
     console.log(sql);
     db.db_connection.query(sql,function (err, result) {
         if(err){
